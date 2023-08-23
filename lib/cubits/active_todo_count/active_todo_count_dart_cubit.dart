08d4ce51a4098b2fedcef00af765999b92cd7227
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-23 21:35:55 +0200

    Add TodoListCubit and StreamSubscryption to ActiveTodoCountCubit to check how much object list have
    To check it we look at TodoListState and filter it using .where and move out object with completed variable as true then we read lenght of rest of objects list
    
    At the end we close stream to close memory leak

--- lib/cubits/active_todo_count/active_todo_count_dart_cubit.dart
+++ lib/cubits/active_todo_count/active_todo_count_dart_cubit.dart
@@ -1,8 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+import 'package:todo_cubit/models/todo_model.dart';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_cubit/cubits/todo_list/todo_list_cubit.dart';
 
 part 'active_todo_count_dart_state.dart';
 
-class ActiveTodoCountDartCubit extends Cubit<ActiveTodoCountDartState> {
-  ActiveTodoCountDartCubit() : super(ActiveTodoCountDartInitial());
+class ActiveTodoCountCubit extends Cubit<ActiveTodoCountState> {
+  final TodoListCubit todoListCubit;
+  late final StreamSubscription todoListSubscryption;
+  ActiveTodoCountCubit({required this.todoListCubit})
+      : super(ActiveTodoCountState.initial()) {
+    todoListSubscryption =
+        todoListCubit.stream.listen((TodoListState todoListState) {
+      print('TodoListState: $todoListState');
+      final int currentActiveTodoCount = todoListState.todos
+          .where((Todo todo) => !todo.completed)
+          .toList()
+          .length;
+      emit(state.copyWith(activeTodoCount: currentActiveTodoCount));
+    });
+    @override
+    Future<void> close() {
+      todoListSubscryption.cancel();
+      return super.close();
+    }
+  }
 }
