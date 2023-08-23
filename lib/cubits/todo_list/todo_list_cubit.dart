4d49d85e8521bd6a9a937387601b3ea839839915
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-23 14:41:37 +0200

    use initial state in cubit create function to add new Todo object (activity to do)

--- lib/cubits/todo_list/todo_list_cubit.dart
+++ lib/cubits/todo_list/todo_list_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_cubit/models/todo_model.dart';
 
 part 'todo_list_state.dart';
 
 class TodoListCubit extends Cubit<TodoListState> {
-  TodoListCubit() : super(TodoListInitial());
+  TodoListCubit() : super(TodoListState.initial());
+
+  void addTodo(String toDoDescryption) {
+    final newTodo = Todo(description: toDoDescryption);
+    final newTodos = [...state.todos, newTodo];
+    emit(state.copyWith(todos: newTodos));
+  }
 }
