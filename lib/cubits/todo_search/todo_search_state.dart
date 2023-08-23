31e2ffb6c05ffe463d84fb5eb1d6797ae16309d2
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-23 14:22:54 +0200

    Add final variable searchText to state and create initial state
    Add toString, copyWith and Equatable

--- lib/cubits/todo_search/todo_search_state.dart
+++ lib/cubits/todo_search/todo_search_state.dart
@@ -1,10 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 part of 'todo_search_cubit.dart';
 
-sealed class TodoSearchState extends Equatable {
-  const TodoSearchState();
+class TodoSearchState extends Equatable {
+  final String searchText;
+  TodoSearchState({
+    required this.searchText,
+  });
+
+  factory TodoSearchState.initial() {
+    return TodoSearchState(searchText: '');
+  }
+
+  TodoSearchState copyWith({
+    String? searchText,
+  }) {
+    return TodoSearchState(
+      searchText: searchText ?? this.searchText,
+    );
+  }
 
   @override
-  List<Object> get props => [];
-}
+  String toString() => 'TodoSearchState(searchText: $searchText)';
 
-final class TodoSearchInitial extends TodoSearchState {}
+  @override
+  List<Object> get props => [searchText];
+}
