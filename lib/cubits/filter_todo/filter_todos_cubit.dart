5a091939f4d165c9377094f8855c73ae85c82586
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-24 21:17:10 +0200

    In FilterTodosCubit add Filter, Search and List Cubits and streams to check it something change there

--- lib/cubits/filter_todo/filter_todos_cubit.dart
+++ lib/cubits/filter_todo/filter_todos_cubit.dart
@@ -1,8 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import 'package:todo_cubit/cubits/todo_filter/todo_filter_cubit.dart';
+import 'package:todo_cubit/cubits/todo_list/todo_list_cubit.dart';
+import 'package:todo_cubit/cubits/todo_search/todo_search_cubit.dart';
+import 'package:todo_cubit/models/todo_model.dart';
+
 part 'filter_todos_state.dart';
 
-class FilterTodosCubit extends Cubit<FilterTodosState> {
-  FilterTodosCubit() : super(FilterTodosInitial());
+class FilterTodosCubit extends Cubit<FilterTodoState> {
+  late final StreamSubscription todoFilterSubscryption;
+  late final StreamSubscription todoSearchSubscryption;
+  late final StreamSubscription todoListSubscryption;
+  final TodoFilterCubit todoFilterCubit;
+  final TodoSearchCubit todoSearchCubit;
+  final TodoListCubit todoListCubit;
+  FilterTodosCubit({
+    required this.todoFilterCubit,
+    required this.todoSearchCubit,
+    required this.todoListCubit,
+  }) : super(FilterTodoState.initial()) {
+    todoFilterSubscryption =
+        todoFilterCubit.stream.listen((TodoFilterState todoFilterState) {});
+
+    todoSearchSubscryption =
+        todoSearchCubit.stream.listen((TodoSearchState todoSearchState) {});
+
+    todoListSubscryption =
+        todoListCubit.stream.listen((TodoListState todoListState) {});
+  }
 }
