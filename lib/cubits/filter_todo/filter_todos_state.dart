33c5e36b07a7825bb0a0b1be4b808fc630cabd0b
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-24 21:09:15 +0200

    Add initial state in FilterTodoState
    Add variable List<Todo> filterTodos
    
    Add toString, equatable, copyWith

--- lib/cubits/filter_todo/filter_todos_state.dart
+++ lib/cubits/filter_todo/filter_todos_state.dart
@@ -1,10 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'filter_todos_cubit.dart';
 
-sealed class FilterTodosState extends Equatable {
-  const FilterTodosState();
+class FilterTodoState extends Equatable {
+  final List<Todo> filterTodos;
+  FilterTodoState({
+    required this.filterTodos,
+  });
+  factory FilterTodoState.initial() {
+    return FilterTodoState(filterTodos: []);
+  }
 
   @override
-  List<Object> get props => [];
-}
+  List<Object> get props => [filterTodos];
 
-final class FilterTodosInitial extends FilterTodosState {}
+  @override
+  bool get stringify => true;
+
+  FilterTodoState copyWith({
+    List<Todo>? filterTodos,
+  }) {
+    return FilterTodoState(
+      filterTodos: filterTodos ?? this.filterTodos,
+    );
+  }
+}
