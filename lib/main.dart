964e3ef1e622440e1c2248da448514543a18c431
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-24 21:54:39 +0200

    Add MultiBlocProvider to inject all cubits to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/cubits.dart';
 import 'package:todo_cubit/pages/todos_page.dart';
 
 void main() {
@@ -11,14 +13,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'TODO',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(
+          create: (context) => TodoFilterCubit(),
+        ),
+        BlocProvider<TodoListCubit>(
+          create: (context) => TodoListCubit(),
+        ),
+        BlocProvider<ActiveTodoCountCubit>(
+            create: (context) => ActiveTodoCountCubit(
+                todoListCubit: BlocProvider.of<TodoListCubit>(context))),
+        BlocProvider<FilterTodosCubit>(
+            create: (context) => FilterTodosCubit(
+                todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+                todoListCubit: BlocProvider.of<TodoListCubit>(context),
+                todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context)))
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'TODO',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const TodoPage(),
       ),
-      home: const TodoPage(),
     );
   }
 }
