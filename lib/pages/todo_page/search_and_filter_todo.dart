5ea0ad7f24e8c65dcd0f6ad56e1e296373a8d3bd
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-25 19:05:41 +0200

    Add text field to search todos using TodoSearchCubit

--- lib/pages/todo_page/search_and_filter_todo.dart
+++ lib/pages/todo_page/search_and_filter_todo.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/todo_search/todo_search_cubit.dart';
 
 class SearchAndFilterTodo extends StatelessWidget {
   const SearchAndFilterTodo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              labelText: 'Search todos..',
+              border: InputBorder.none,
+              filled: true,
+              prefixIcon: Icon(
+                Icons.search,
+              )),
+          onChanged: (newSearchText) {
+            if (newSearchText != null) {
+              context.read<TodoSearchCubit>().searchActivities(newSearchText);
+            }
+          },
+        ),
+        SizedBox(
+          height: 10,
+        )
+      ],
+    );
   }
 }
