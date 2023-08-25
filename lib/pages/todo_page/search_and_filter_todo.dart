74f9cadb364741d196818b2fa2960d06441760a4
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-25 18:55:57 +0200

    Create empty search and filter widget and add this to todos page

---
+++ lib/pages/todo_page/search_and_filter_todo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchAndFilterTodo extends StatelessWidget {
+  const SearchAndFilterTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/todo_page/todos_page.dart
+++ lib/pages/todo_page/todos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_cubit/pages/todo_page/create_todo.dart';
+import 'package:todo_cubit/pages/todo_page/search_and_filter_todo.dart';
 import 'package:todo_cubit/pages/todo_page/todo_header.dart';
 
 class TodoPage extends StatelessWidget {
@@ -13,7 +14,14 @@ class TodoPage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
             child: Column(
-              children: [TodoHeader(), CreateTodo()],
+              children: [
+                TodoHeader(),
+                CreateTodo(),
+                SizedBox(
+                  height: 20,
+                ),
+                SearchAndFilterTodo()
+              ],
             ),
           ),
         ),
