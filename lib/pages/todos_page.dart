473848dbed96863050cb74925a0004bbb487f1e3
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-25 12:47:36 +0200

    Remove appbar, add SafeArea and SingleChildScrollView
    Create TodoHeader widget, row with two texts

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -5,17 +5,38 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          'TODO App',
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+            child: Column(
+              children: [TodoHeader()],
+            ),
+          ),
         ),
       ),
-      body: Center(
-        child: Text('TODOS'),
-      ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'TODO',
+          style: TextStyle(fontSize: 40),
+        ),
+        Text(
+          '0 items left',
+          style: TextStyle(fontSize: 20, color: Colors.redAccent),
+        )
+      ],
     );
   }
 }
